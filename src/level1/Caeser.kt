8858d4ba242766f8034bf634c5f46df92f337ccc
Joonhoo <mnc5@naver.com>
2022-05-24 21:43:57 +0900

    220524

---
+++ src/level1/Caeser.kt
@@ -0,0 +1,25 @@
+package level1
+
+class Caeser {
+    fun solution(s: String, n: Int): String {
+        return s.toList().joinToString("") {
+            when (it) {
+                in 'A'..'Z' -> {
+                    if (it + n > 'Z') {
+                        it + n - 26
+                    } else {
+                        it + n
+                    }
+                }
+                in 'a'..'z' -> {
+                    if (it + n > 'z') {
+                        it + n - 26
+                    } else {
+                        it + n
+                    }
+                }
+                else -> it
+            }.toString()
+        }
+    }
+}
