c11a8222337291674339f4f1eaab8bd279a28066
Joonhoo <mnc5@naver.com>
2022-05-27 23:12:20 +0900

    220527

---
+++ src/level1/Money.kt
@@ -0,0 +1,14 @@
+package level1
+
+class Money {
+    fun solution(price: Int, money: Int, count: Int): Long {
+        val answer: Long
+        var total: Long = 0
+        // 탄 횟수별로 가격 정하기
+        for (i in 1..count) {
+            total += price.toLong() * i
+        }
+        answer = total - money
+        return if (answer < 0) 0 else answer
+    }
+}
