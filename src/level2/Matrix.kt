1bd43e729e316f6eca6d15bf0e32fc889e6c3ea0
Joonhoo <mnc5@naver.com>
2022-06-26 23:58:29 +0900

    220626

---
+++ src/level2/Matrix.kt
@@ -0,0 +1,15 @@
+package level2
+
+class Matrix {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+        val answer = Array(arr1.size) { IntArray(arr2[0].size) }
+        for (row in arr1.indices) {
+            for (col in 0 until arr2[0].size) {
+                for (k in arr2.indices) {
+                    answer[row][col] += arr1[row][k] * arr2[k][col]
+                }
+            }
+        }
+        return answer
+    }
+}
