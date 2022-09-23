f15ce8d231bc911182ae76f2a7fe200061b487d6
Joonhoo <mnc5@naver.com>
2022-09-24 01:44:57 +0900

    220924

---
+++ src/programmers/kakao/KAKAO_BLIND_2021_합승택시요금.kt
@@ -0,0 +1,32 @@
+package programmers.kakao
+
+class KAKAO_BLIND_2021_합승택시요금 {
+    private val INF = 3000000
+    fun solution(n: Int, s: Int, a: Int, b: Int, fares: Array<IntArray>): Int {
+        var answer = INF
+        val dist = Array(n + 1) { IntArray(n + 1) { INF } }
+        for (i in 1..n) {
+            dist[i][i] = 0
+        }
+        // 택시 요금 Init
+        for (fare in fares) {
+            dist[fare[0]][fare[1]] = fare[2]
+            dist[fare[1]][fare[0]] = fare[2]
+        }
+        // 플로이드
+        for (k in 1..n) {
+            for (i in 1..n) {
+                for (j in 1..n) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j]
+                    }
+                }
+            }
+        }
+        // a, b
+        for (k in 1..n) {
+            answer = answer.coerceAtMost(dist[s][k] + dist[k][a] + dist[k][b])
+        }
+        return answer
+    }
+}
