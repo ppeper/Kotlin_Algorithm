84e0b056a20870dd400e5fb52400b7d573d9cbae
Joonhoo <mnc5@naver.com>
2022-10-02 03:34:53 +0900

    221002

---
+++ src/boj/잃어버린 괄호_1541.kt
@@ -0,0 +1,21 @@
+package boj
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main(args: Array<String>) = with (BufferedReader(InputStreamReader(System.`in`))) {
+    // - 기준으로 나눔
+    val form = readLine().split("-".toRegex())
+    var result = 0
+    for (i in form.indices) {
+        val plus = form[i].split("\\+".toRegex())
+        for (num in plus) {
+            if (i == 0) {
+                result += num.toInt()
+            } else {
+                result -= num.toInt()
+            }
+        }
+    }
+    println(result)
+}
