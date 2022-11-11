0322040bb8f624cd4d700fcdcc2e90ef3d1cc251
Joonhoo <mnc5@naver.com>
2022-11-11 21:54:46 +0900

    221111

---
+++ src/boj/최소 힙_1927.kt
@@ -0,0 +1,24 @@
+package boj
+
+import java.util.PriorityQueue
+
+fun main() {
+    val count = readln().toInt()
+    val pq = PriorityQueue<Int>(compareBy { it })
+    val answer = ArrayList<Int>()
+    for (i in 0 until count) {
+        val number = readln().toInt()
+        if (number == 0) {
+            if (pq.isEmpty()) {
+                answer.add(0)
+            } else {
+                answer.add(pq.poll())
+            }
+        } else {
+            pq.add(number)
+        }
+    }
+    answer.forEach {
+        println(it)
+    }
+}
