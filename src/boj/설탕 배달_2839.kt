16cb6d633d2bacf83e69f5297abc7170f13b2a15
Joonhoo <mnc5@naver.com>
2022-11-12 23:53:10 +0900

    221112

---
+++ src/boj/설탕 배달_2839.kt
@@ -0,0 +1,18 @@
+package boj
+
+fun main() {
+    var size = readln().toInt()
+    var count = 0
+    while (true) {
+        if (size % 5 == 0) {
+            println("${size / 5 + count}")
+            break
+        }
+        if (size - 3 < 0) {
+            println(-1)
+            break
+        }
+        size -= 3
+        count++
+    }
+}
