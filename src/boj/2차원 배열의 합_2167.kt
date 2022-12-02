2982a397fc613f4c4a4c7cfd7a2947e1ae20a7d0
Joonhoo <mnc5@naver.com>
2022-12-02 16:04:47 +0900

    221202

---
+++ src/boj/2차원 배열의 합_2167.kt
@@ -0,0 +1,29 @@
+package boj
+
+fun main() {
+    val (N, M) = readLine()!!.split(' ').map { it.toInt() }
+    val list = Array(N) { IntArray(M) }
+    for (i in 0 until N) {
+        list[i] = readLine()!!.split(' ').map { it.toInt() }.toIntArray()
+    }
+    val sum = Array(N + 1) { IntArray(M + 1) }
+    // 누적합 구하기
+    for (i in list.indices) {
+        for (j in list[i].indices) {
+            sum[i + 1][j + 1] = sum[i][j + 1] + sum[i + 1][j] - sum[i][j] + list[i][j]
+        }
+    }
+    val count = readln().toInt()
+    val answer = IntArray(count)
+    for (j in 0 until count) {
+        val (x1, y1, x2, y2) = readLine()!!.split(' ').map { it.toInt() }
+        answer[j] = getSum(x1, y1, x2, y2, sum)
+    }
+    answer.forEach {
+        println(it)
+    }
+}
+
+private fun getSum(x1: Int, y1: Int, x2: Int, y2: Int, sum: Array<IntArray>): Int {
+    return sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1]
+}
