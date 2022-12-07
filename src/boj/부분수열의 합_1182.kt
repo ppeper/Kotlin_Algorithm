5e10bf4390538064a9b10ef8756c779de5ad3f15
Joonhoo <mnc5@naver.com>
2022-12-07 15:33:09 +0900

    221207

---
+++ src/boj/부분수열의 합_1182.kt
@@ -0,0 +1,20 @@
+package boj
+
+var count2 = 0
+var S = 0
+fun main() {
+    val (N, s) = readLine()!!.split(' ').map { it.toInt() }
+    S = s
+    val list = readLine()!!.split(' ').map { it.toInt() }
+    makeSequence(list, 0, 0)
+    if (S == 0) println(count2 - 1) else println(count2)
+}
+
+private fun makeSequence(list: List<Int>, index: Int, sum: Int) {
+    if (index == list.size) {
+        if (sum == S) count2++
+        return
+    }
+    makeSequence(list, index + 1, sum)
+    makeSequence(list, index + 1, sum + list[index])
+}
